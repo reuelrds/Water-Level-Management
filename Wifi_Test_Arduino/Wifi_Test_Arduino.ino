c0ae60885b20251eb9600dcfe1f9dd4ca1fe5795
Reuel D'silva <reuel678@gmail.com>
2018-09-19 21:46:22 +0530

    Wifi Test: Added comments

--- Wifi_Test_Arduino/Wifi_Test_Arduino.ino
+++ Wifi_Test_Arduino/Wifi_Test_Arduino.ino
@@ -1,37 +1,121 @@
-//#include <AltSoftSerial.h>
+// Note:  This Sketch is to be uploaded to Arduino board
+
+/*****
+  Including Libraries
+*****/
 #include <SoftwareSerial.h>
 #include <Ticker.h>
 #include <TimerOne.h>
 
-//AltSoftSerial espSerial;
+
+/*******************************************************************
+  Declaring and initializing variables & Objects
+********************************************************************/
+
+/*****
+  Setting up SoftwareSerial object 
+*****/
+
+// RX port as Digital pin 9 and 
+// TX port as digital pin 8
 SoftwareSerial espSerial(9,8);
 
+
+/*****
+ Initializing Ticker Objects
+*****/
+
+// Printer checks every 500milliseconds to see
+// if a string was received from ESP Module
 Ticker printer(printString, 500);
+
+// sendData sends data to ESP every 20seconds 
+// when the connection to Internet is Established
 Ticker sender(sendData, 20000);
+
+// Setting up ESP to Connect to a WiFi Access Point
+// It fires a connection request string <ConnectToWlan> every 5seconds
+// untill the Esp responds with acknowlwdgement string <Connecting>
 Ticker setupEsp(setupWlan, 5000);
+
+// Sends a acknowledgement string <ConnectReqAck> to Esp once it receives
+// <Connecting> string untill the Esp responds with <Connected>
+// whicch signifies that the Esp has connected to Internet successfully
+// note: we change the frequency from 5sec to 30sec after sending ack string for first time.
 Ticker ackEsp(ackConn, 5000);
 
+
+/*****
+  Variables for receiving data from Esp
+*****/
+// str:   used to store the received string
+// c:     used to store a single incomming character which is then appended to str
+// flag:  used to signify if we have received a complete string or not
 String str = "";
 char c;
 int flag = 0;
 
+/*****
+  Variable to store data being sent to Esp
+*****/
+// val: is used to as a dummy data to be uploaded to ThingSpeak,
+//      once connection to Internet has been established
 int val = 0;
 
+/*****
+  Flags for Setting up connection
+*****/
+// isConn:  This flag tells if the Esp has connected to Internet
+//          It is set after receiving <Connected> string
+// ack:     This flag tells if the Esp has acknowledged the <ConnectToWlan> Flag
+//          It is set after receiving <Connecting> string
 int isConn = 0;
 int ack = 0;
 
-unsigned long prevTime = 0;
-unsigned long currTime;
 
+/*******************************************************************
+  Initial Setup
+********************************************************************/
 void setup() {
-  // put your setup code here, to run once:
+
+/*****
+  Setting Up ISR for Reading Characters from ESP
+*****/
+
+// TimerOne Library uses Timer interrupts to read characters received from Esp
+// It interrupts the sketch every 1millisecond and executes readChar function 
+// to read Characters received from esp if there are any
+
+// Note:  The interval of 1 millicscond or 1000 microseconds is choosen because
+//        the Arduino and Esp communicate at a baud rate of 9600bps.
+//        At this baud rate, a single character (ie. a byte of data. As ASCII characters are represented by 8 bits),
+//        requires roughly 833.33 microseconds to be transfered between Esp and Arduino
   Timer1.initialize(1000);
   Timer1.attachInterrupt(readChar);
   delay(50);
-  
+
+/*****
+  Setting up Serial Connections
+*****/
+// Initialize the Serial Monitor at a baud rate of 115200bps 
+// to print data to serial monitor as soon as possible.
+// Note: Higher baud can also be used
   Serial.begin(115200);
+
+// Initialize Serial Connection between Arduino and ESP at a baud rate of 9600bps
+// Note:  The reason for choosing this particular baud rate was specified in the above TimerOne Note.
+//        And also lower baud rate ensures less errors.
   espSerial.begin(9600);
   delay(100);
+
+/*****
+  Starting Ticker Objects
+*****/
+
+// Note:  Since Ticker Library doesn't specify a status Flag when we initialize the object,
+//        we cannot call TickerObj.status() when it hasn't been started.
+//        Here, Some ticker objects are paused right after they are started because we need
+//        them to start/fire only after some event happens.
 
   sender.start();
   sender.pause();
@@ -39,23 +123,25 @@ void setup() {
   ackEsp.pause();
   printer.start();
   setupEsp.start();
-  
-  
-//  espSerial.print("Start Transmit\r");
-
-  
 }
 
+
+/*******************************************************************
+  Main Loop
+********************************************************************/
 void loop() {
-  // put your main code here, to run repeatedly:
+
+/*****
+  Here we check for events and fire appopriate methods to handle them
+*****/
 
   if (ack == 0 && isConn == 0) {
-    
     setupEsp.update();  
-    
   } else if ( ack == 1 && isConn == 0) {
     
-    setupEsp.stop();
+    // Stop SetupEsp ticker Object once the Esp responds with <Connecting> string
+    // and start sending acknowledgement
+    setupEsp.stop();                        
     if (ackEsp.state() == PAUSED) {
       ackEsp.resume();
     } else {
@@ -63,20 +149,31 @@ void loop() {
     } 
     
   } else if (ack == 1 && isConn == 1) {
+
+    // Stop ackEsp ticker Object once the Esp responds with <Connected> string
+    // and start sending dummy Data
     ackEsp.stop();
     if (sender.state() == PAUSED) {
       sender.resume();
     } else {
       sender.update();    
     }
-  
+    
   }
+
   printer.update();
 
+  // Change interval of sending ack String to Esp
+  // to 30 sec as connecting to wifi may take some time.
   if(ackEsp.counter() == 1) {
     ackEsp.interval(30000);
   }
 }
+
+/*******************************************************************
+  Callback Functions
+********************************************************************/
+
 
 void setupWlan() {
   espSerial.print("<ConnectToWlan>\r");
@@ -86,6 +183,7 @@ void setupWlan() {
 void ackConn() {
   espSerial.print("<ConnectReqAck>\r");
 }
+
 
 void sendData() {
   espSerial.print(String(val) + "\r");
@@ -104,7 +202,7 @@ void printString() {
       ackEsp.stop();
       isConn = 1;
     }
-    if (str == ".") {
+    if (str == ".") {       // Special condition for while waiting to get IP adderss from router
       Serial.print(str);
     } else {
       Serial.println(str);
@@ -114,17 +212,16 @@ void printString() {
     flag = 0;
     Timer1.restart();
   }
-//  Serial.println("ACK: " + String(ack) + " COnn: " + String(isConn));
 }
 
 void readChar(){
   if(espSerial.available() > 0) {
     c = (char)espSerial.read();
     
-    if (c == '\r') {
+    if (c == '\r') {                  // '\r' is being used as a deliminator to siginify end of String
       flag = 1;
     } else {
-      if (c <= 126 && c >= 32) {
+      if (c <= 126 && c >= 32) {      // Helps to discard any garbled data.
         str.concat(c);
       }
     }
--- Wifi_Test_Esp01/Wifi_Test_Esp01.ino
+++ Wifi_Test_Esp01/Wifi_Test_Esp01.ino
@@ -1,37 +1,102 @@
+// Note:  This Sketch needs to be Uploaded to the Esp8266 Module
+
+/*****
+  Including Libraries
+*****/
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ThingSpeak.h>
 #include <Ticker.h>
 
+
+/*******************************************************************
+  Declaring and initializing variables & Objects
+********************************************************************/
+
+/*****
+  Declaring Ticker Object
+*****/
 Ticker reader;
 
-
+/*****
+  Declaring WiFi ssid and password
+*****/
+// Note:  This ssid and password are for my phones hotspot.
+//        These values will need to be changed when using some other WiFi AP
 const char* ssid = "ReuelRds";
 const char* password = "reuelrds1234";
 
+/*****
+  Variables uesd to connect and upload data to ThingSpeak Cloud
+*****/
+// Note:  The private key and channel number are related to my ThingSpeak account
+//        These will need to be changed when we deploy the prototype model
 const char* host = "api.thingspeak.com";
 const char* privateKey = "09E2POZ8LM87VIBT";
 const int channelNumber = 570672;
 
-int flag = 0;
+
+/*****
+  Variables for receiving data from Arduino
+*****/
+// str:   used to store the received string
+// c:     used to store a single incomming character which is then appended to str
+// flag:  used to signify if we have received a complete string or not
+
 String str = "";
 char c;
+int flag = 0;
 
+/*****
+  Flags for Setting up connection
+*****/
+// isConn:  This flag tells if the Esp has connected to Internet
+//          and avoids call connectWlan() method once again.
 int isConn = 0;
 
+/*****
+  Variable to store data being sent to ThingSpeak
+*****/
+// val: is used to as a dummy data to be uploaded to ThingSpeak,
+//      once connection to Internet has been established.
 int val = 0;
+
+
+/*******************************************************************
+  Initial Setup
+********************************************************************/
+
 void setup() {
-  // put your setup code here, to run once:
+/*****
+  Setting up Serial Connection with Arduino
+*****/
+// Initialize Serial Connection between Arduino and ESP at a baud rate of 9600bps
+// Note:  The reason for choosing this particular baud rate was specified in the TimerOne Note in Arduino Sketch
+//        And also lower baud rate ensures less errors.
   Serial.begin(9600);
   delay(100);
 
+/*****
+  Disconnect from WiFi AP
+*****/
+// Note:  We Don't need Esp sending data unless Arduino sends it after it knows Esp is Connected to Internet
   WiFi.disconnect();
 
+/*****
+  Starting Ticker Object
+*****/
+// fire readChar function every 1 millisecond to capture character received from Arduino.
+// Note:  The reason for choosing this time interval was specified in the TimerOne Note in Arduino Sketch
   reader.attach_ms(1, readChar);
 }
 
+
+
+/*******************************************************************
+  Main Loop
+********************************************************************/
+
 void loop() {
-  // put your main code here, to run repeatedly:
 
   if (flag == 1) {
     reader.detach();
@@ -40,16 +105,10 @@ void loop() {
       Serial.print("<Connecting>\r");
       delay(500);
     } else if (str == "<ConnectReqAck>") {
-      if (isConn == 1) {
-//        Serial.print("<Connected>");
-//        Serial.print('\r');
-//        delay(500);
-      } else {
+      if (isConn != 1) {
         connectWlan();  
       }
-      
-      
-    } else {
+    } else {                // At this point the Esp is Connected to Internet and we can start uploading data
       val = str.toInt();
       uploadValues();
     }
@@ -59,28 +118,22 @@ void loop() {
   }
 }
 
-void readChar() {
-  if (Serial.available() > 0) {
-    c = (char)Serial.read();
-    if (c == '\r') {
-      flag = 1;
-    } else {
-      str.concat(c);
-    }
-  }
-}
 
 
+/*******************************************************************
+  Establish COnnection to Wifi Access Point
+********************************************************************/
 void connectWlan() {
 
-//  Serial.println();
-//  Serial.println();
   Serial.print("Connecting to ");
   Serial.print(ssid);
   Serial.print("\r");
 
   delay(1000);
 
+  // Set Esp in Station mode (WiFi Client) mode, otherwise, it by default,
+  // would try to act as both a client and an access-point and could cause
+  // network-issues with your other WiFi-devices on your WiFi-network.
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   WiFi.begin(ssid, password);
@@ -113,18 +166,39 @@ void connectWlan() {
   delay(500);
 }
 
+/*******************************************************************
+  Uploading values to ThingSpeak Client
+********************************************************************/
 void uploadValues() {
-  // Use WiFiClient class to create TCP connections
+
+  // Note:  Need to test if all these statements except 
+  //        setField and writeFields can be moved to setup block.
   
+  // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const int httpPort = 80;
   if (!client.connect(host, httpPort)) {
     Serial.print("connection failed\r");
   }
 
+  // Uploading the value received from Arduino
   ThingSpeak.begin(client);
   ThingSpeak.setField(1, val);
   ThingSpeak.writeFields(channelNumber, privateKey);
   Serial.print("Sent val: " + String(val) + "\r");
   delay(1000);
+}
+
+/*******************************************************************
+  Callback function to read a Character received from Arduino
+********************************************************************/
+void readChar() {
+  if (Serial.available() > 0) {
+    c = (char)Serial.read();
+    if (c == '\r') {
+      flag = 1;
+    } else {
+      str.concat(c);
+    }
+  }
 }
